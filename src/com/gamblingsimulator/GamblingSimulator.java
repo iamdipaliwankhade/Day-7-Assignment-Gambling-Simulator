9f032b10c090a28d64b8b1d018a2a6f069661791
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-12 14:09:42 +0530

    [Dipali] Added : stake per game

---
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,15 @@
+package com.gamblingsimulator;
+/**
+ * 
+ * @param args
+ */
+public class GamblingSimulator {
+	public static final int initialStake = 100;
+	public static final int stakeBet = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Game Simulator program");
+		System.out.println("Initial Stake is " + initialStake + "$");
+		System.out.println("Stake Bet is " + stakeBet + "$");
+	}
+}
